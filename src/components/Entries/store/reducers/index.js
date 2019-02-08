3ffc21a97737836a240d5792b25fbb51befed054
Eddie Padin <eddie@entertainermedia.net>
2019-02-08 16:57:05 -0600

    refactor: cleanup entries reducer;

--- src/components/Entries/store/reducers/index.js
+++ src/components/Entries/store/reducers/index.js
@@ -1,9 +1,5 @@
-import { combineReducers } from "redux";
 import entries from "./entries.reducer";
 
-// const reducers = combineReducers({
-//   entries
-// });
 const reducers = entries;
 
 export default reducers;
